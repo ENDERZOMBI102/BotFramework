4800c69b32c552a90da36f3653e8992b4db12d35
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-03-15 17:23:26 +0100

    make listeners private @Listen -> @Listener add docs to @Listener decorator
    Signed-off-by: ENDERZOMBI102 <enderzombi102.end@gmail.com>

--- src/botframework/eventSystem.py
+++ src/botframework/eventSystem.py
@@ -1,52 +1,62 @@
-from typing import Dict, List
+from typing import Dict
 
+from botframework.abc.eventSystem import AbstractEventSystem
 from botframework.logging import get_logger
 from botframework.types import ListenerList, Event, Coroutine
 
 logger = get_logger('EventSystem')
 
 
-class EventSystem:
+class EventSystem(AbstractEventSystem):
 
 	INSTANCE: 'EventSystem'
-	listeners: Dict[Event, ListenerList]
+	_listeners: Dict[ Event, ListenerList ]
 
 	def __init__(self):
 		EventSystem.INSTANCE = self
 
-	def removeListener( self, module: str ):
+	def removeListeners( self, module: str ):
 		# cycle in all event lists
-		for listenerList in self.listeners.values():
+		for listenerList in self._listeners.values():
 			toRemove = []
 			for listener in listenerList:
 				if listener.__module__ == module:
-					toRemove.append( listener )  # schedule for removal all listeners of module module
+					toRemove.append( listener )  # schedule for removal all _listeners of module module
 			for func in toRemove:
-				listenerList.remove(func)  # remove all found listeners
+				listenerList.remove(func)  # remove all found _listeners
 
 	def addListener( self, listener: Coroutine, event: Event ):
 		# check if the event list exists
-		if event not in self.listeners.keys():
+		if event not in self._listeners.keys():
 			# if not, create it
-			self.listeners[ event ] = [ ]
+			self._listeners[ event ] = [ ]
 		# add the listener
-		self.listeners[ event ].append( listener )
+		logger.info( f'Module "{listener.__module__}" registered listener for event "{event}".' )
+		self._listeners[ event ].append( listener )
 
 	async def invoke( self, event: Event, **kwargs ):
-		if event not in self.listeners.keys():
-			self.listeners[ event ] = []
+		if event not in self._listeners.keys():
+			self._listeners[ event ] = []
 
-		if len( self.listeners[event] ) == 0:
-			logger.warning(f'Invoked event "{event}" has no listeners!')
+		if len( self._listeners[event ] ) == 0:
+			logger.warning(f'Invoked event "{event}" has no _listeners!')
 
-		for listener in self.listeners[event]:
+		for listener in self._listeners[event ]:
 			await listener(**kwargs)
 
 
 # event listeners should be named:
 # onEventName
 
-def Listen(func: Coroutine):
+def Listener(func: Coroutine):
+	"""
+	Decorator for event listeners.
+	listeners should be named after the event they are listening for, ex:
+	@Listener
+	async def onMessage(server: AbstractServer, msg: Message):
+		pass
+	:param func: the listener
+	"""
 	event: str = func.__code__.co_name[2:].lower()
 	# add the listener
 	EventSystem.INSTANCE.addListener(func, event)
